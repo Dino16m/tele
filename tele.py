f8e5b33bd9b05dd286ecabb5de0600165895c826
dino16m <anselem16m@gmail.com>
2019-08-25 22:38:32 +0100

    Refactor2

--- tele.py
+++ tele.py
@@ -334,5 +334,6 @@ def joinChannel(peters, channelName):
 
 if __name__ == '__main__':
     peters = ['dynasties', '12', '13', '5', '7']
-    #joinChannel(peters, 'successvisa')  'james', 'john', 'mary', 'mike', 'mike10', 'mike20', 'mike4'
+    joinChannel(peters, 'successvisa')  
+    #'james', 'john', 'mary', 'mike', 'mike10', 'mike20', 'mike4'
     add(peters, 'successvisa', getFrom=['TodayILearn', 'american'])
