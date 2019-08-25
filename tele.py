74869ad16aef00a1029c6c6c65ba9eccb8044da3
dino16m <anselem16m@gmail.com>
2019-08-25 21:58:05 +0100

    increment

--- tele.py
+++ tele.py
@@ -1,21 +1,13 @@
-import random
-from telethon.sync import TelegramClient
-from telethon.tl.functions.channels import JoinChannelRequest
-from telethon.tl.functions.contacts import ResolveUsernameRequest
-from telethon.tl.functions.channels import GetAdminLogRequest
-from telethon.tl.functions.channels import GetParticipantsRequest
-from time import sleep
-from telethon.tl.types import ChannelParticipantsRecent
-from telethon.tl.types import InputChannel
-from telethon.tl.types import ChannelAdminLogEventsFilter
-from telethon.tl.types import InputUserSelf
-from telethon.tl.types import InputUser
-from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser, PeerUser
-from singlify import makeSingle, readFromFile, writeToFile, writeSingle, backupUsers, commitSuccess
 import os
-from vomitonegro import do
-from telethon.tl.functions.channels import InviteToChannelRequest
+import random
+from time import sleep
 from myexceptions import OutOfUserException
+from singlify import backupUsers, readFromFile, writeSingle, writeToFile
+from telethon.sync import TelegramClient
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.types import InputPeerChannel
+from vomitonegro import do
 
 
 #api_id = 872129
@@ -24,11 +16,13 @@ from myexceptions import OutOfUserException
 api_id = 764531
 api_hash = 'a41f8549c7dd1341613de3569f9796cb'
 
+
 def getChannels(client):
     channels = {d.entity.username: d.entity
-        for d in client.get_dialogs()
-            if d.is_channel}
+                for d in client.get_dialogs()
+                if d.is_channel}
     return channels
+
 
 def getChannelParticipants(client, channel):
     users = []
@@ -40,28 +34,31 @@ def getChannelParticipants(client, channel):
     except Exception as e:
         print(e.args)
     return users
-    
+
+
 def addUsersToCsv(users, filepath, singlify=False):
     basename = os.path.basename(filepath)
     basedir = os.path.dirname(filepath)
     if singlify:
-        return writeSingle(filepath, users) 
+        return writeSingle(filepath, users)
     else:
         return writeToFile(users, basename, basedir)
 
-def chunkify(list, chunkSize=15):
-    chunks = []
+
+def chunkify(list, chunkSize=100):
+    chunks = [[]]
     count = 0
-    tempList = []
     for list1 in list:
         if count < chunkSize:
-            tempList.append(list1)
+            chunk = chunks[-1]
             count = count + 1
         else:
-            chunks.append(tempList)
-            tempList = []
+            chunks.append([])
+            chunk = chunks[-1]
             count = 0
+        chunk.append(list1)
     return chunks
+
 
 def getUserEntities(client, users):
     list = []
@@ -77,6 +74,7 @@ def getUserEntities(client, users):
             list.append(userToAdd)
     return chunkify(list)
 
+
 def convertEntity(client, entities):
     returnable = []
     for entity in entities:
@@ -84,6 +82,7 @@ def convertEntity(client, entities):
         if user.username is not None:
             returnable.append(user.username)
     return returnable
+
 
 def getSuccessFromUpdate(update):
     if len(update) < 1:
@@ -94,10 +93,12 @@ def getSuccessFromUpdate(update):
             users.append(u.username)
     return users
 
+
 def printAddStatus(length):
     if length > 0:
-        print('The count here is '+str(length))
+        print('The count here is ' + str(length))
         print('adding users to channel')
+
 
 def addUsersToChannel(client, users, channel):
     channelEntity = InputPeerChannel(channel.id, channel.access_hash)
@@ -105,10 +106,10 @@ def addUsersToChannel(client, users, channel):
     error = False
     success = []
     usersToAdd = getUserEntities(client, users)
-    for usersToAdd1 in usersToAdd:       
+    for usersToAdd1 in usersToAdd:
         try:
             update = client(InviteToChannelRequest(channelEntity, usersToAdd1))
-        
+
         except Exception as e:
             print(e.args)
             error = True
@@ -119,13 +120,15 @@ def addUsersToChannel(client, users, channel):
             error = False
         if count >= 50:
             tick = random.randint(10, 70) * 2
-            print('adding has been paused for '+str(tick)+' seconds in line with Telegram guidelines, it will resume immediately the time elapses.')
+            print('adding has been paused for '+str(tick) +
+                  ' seconds in line with Telegram guidelines, it will resume immediately the time elapses.')
             sleep(tick)
             count = 0
             continue
     count = count + 1
     return success
-    
+
+
 def getUsersFromCsv(filepath):
     basename = os.path.basename(filepath)
     basedir = os.path.dirname(filepath)
@@ -133,10 +136,12 @@ def getUsersFromCsv(filepath):
         return []
     return readFromFile(basename, basedir)
 
+
 def printChannels(channels):
     for channel in channels:
         print(channel)
     return
+
 
 def getSomeUsers(range, users):
     list = []
@@ -147,9 +152,10 @@ def getSomeUsers(range, users):
         list.append(user)
         i = i + 1
     return list
-                
+
+
 def getAllChannelUsers(client, channels):
-    list = [] 
+    list = []
     for channel in channels:
         if channel is None:
             continue
@@ -157,26 +163,31 @@ def getAllChannelUsers(client, channels):
         list.extend(users)
     return list
 
+
 def removeSuccess(success, users):
     setWithoutSuccess = set(users) - set(success)
     listWithoutSuccess = list(setWithoutSuccess)
     return listWithoutSuccess
-    
+
+
 def numberOfUsers(filepath):
-    if not os.path.isfile(filepath): 
+    if not os.path.isfile(filepath):
         return 0
     basename = os.path.basename(filepath)
     basedir = os.path.dirname(filepath)
     return len(readFromFile(basename, basedir))
 
+
 def backup(filepath):
     return backupUsers(filepath)
+
 
 def finalResolve(error, count=0, limit=0):
     if __name__ == '__main__':
         exit(error)
     else:
         return {'status': False, 'error': error}
+
 
 def removeUsersAlreadyInChannel(channelUsers, users):
     channelUsersList = []
@@ -189,6 +200,7 @@ def removeUsersAlreadyInChannel(channelUsers, users):
             channelUsersList.append(channelUser.username)
     unique = set(users) - set(channelUsersList)
     return list(unique)
+
 
 def getUsers(source, channels={}, online=True, getFrom=[], usedChannels=[]):
     users = []
@@ -212,29 +224,32 @@ def getUsers(source, channels={}, online=True, getFrom=[], usedChannels=[]):
         users = getUsersFromCsv(source)
     return users
 
+
 def untappedAddingPotential(peterLnt, count, limit, userLnt):
     if count < limit:
         return True
     weight = 10
     if count < limit:
-        weight+=1
+        weight += 1
     else:
-        weight-=1
+        weight -= 1
     if (peterLnt * 150) < count:
-        weight+=1
+        weight += 1
     else:
-        weight-=1
+        weight -= 1
     if (userLnt-count) > 50:
-        weight+=1
+        weight += 1
     if weight >= 10:
         return True
     return False
 
+
 def add(peters, channelInto, online=True, getFrom=[], filepath='users.txt', limit=1000):
-    #do()
+    # do()
     count = 0
     print('adding has started')  # delete in production
-    users = [] #(lambda: getUsers(filepath, online=False), lambda: [])[online is True]()
+    # (lambda: getUsers(filepath, online=False), lambda: [])[online is True]()
+    users = []
     removedUsersInChannel = False
     usedChannels = []
     trials = True
@@ -243,32 +258,37 @@ def add(peters, channelInto, online=True, getFrom=[], filepath='users.txt', limi
             with TelegramClient(peter, api_id, api_hash) as client:
                 #client.send_message('me', 'Hello, myself!')
                 channels = getChannels(client)
-                tmpUsers = (lambda: users, lambda: getUsers(client, channels, online, getFrom, usedChannels))[online is True]()
+                tmpUsers = (lambda: users, lambda: getUsers(
+                    client, channels, online, getFrom, usedChannels))[online is True]()
                 users = list(set(users + tmpUsers))
-                print('for peter '+peter+' the users before are: '+ str(len(users)))
+                print('for peter '+peter +
+                      ' the users before are: ' + str(len(users)))
                 if not users and count <= limit:
                     continue
                 printChannels(channels)  # delete in production
                 if channelInto not in channels.keys():
                     continue
-                if channelInto not in channels.keys() and peters[len(peters)-1] == peter: 
-                    return finalResolve('invalid channel name') 
+                if channelInto not in channels.keys() and peters[len(peters)-1] == peter:
+                    return finalResolve('invalid channel name')
                 if not removedUsersInChannel:
-                    channelUsers = getChannelParticipants(client, channels[channelInto])
+                    channelUsers = getChannelParticipants(
+                        client, channels[channelInto])
                     users = removeUsersAlreadyInChannel(channelUsers, users)
-                    print('after removing users from the channel, they are now: '+str(len(users)))
+                    print(
+                        'after removing users from the channel, they are now: '+str(len(users)))
                     removedUsersInChannel = True
                 if count < limit:
-                    success = addUsersToChannel(client, users, channels[channelInto])
+                    success = addUsersToChannel(
+                        client, users, channels[channelInto])
                     count = count + len(success)
                 else:
                     return finalResolve('you have exhausted your quota and adding has ended.')
-                if not users and (peters[len(peters)-1] == peter and count<limit):
+                if not users and (peters[len(peters)-1] == peter and count < limit):
                     raise OutOfUserException(count, limit)
                 users = removeSuccess(success, users)
                 random.shuffle(users)
                 print('successes are '+str(len(success)))
-                print('users after accounting for successes are '+ str(len(users)))
+                print('users after accounting for successes are ' + str(len(users)))
                 print('peter '+peter+' done and dusted adding.')
             removedUsersInChannel = False
             trials = False
@@ -277,13 +297,13 @@ def add(peters, channelInto, online=True, getFrom=[], filepath='users.txt', limi
             random.shuffle(peters)
             continue
     print('adding has ended.')
-    report = {'status': True, 'data': {'usersAdded': count, 'channelAddedTo': channelInto}}
+    report = {'status': True, 'data': {
+        'usersAdded': count, 'channelAddedTo': channelInto}}
     return report
 
+
 def work(peters, getFrom=[], filepath='users.txt'):
-    """
-    This method stores users from channels channels specified by the getFrom param avove to a given file.
-    """
+    """The method stores users from channels channels specified by the getFrom param avove to a given file."""
     # do()
     print('storing users started')
     allUsers = []
@@ -295,22 +315,24 @@ def work(peters, getFrom=[], filepath='users.txt'):
             channels = getChannels(client)
             printChannels(channels)
             users = getUsers(client, channels, True, getFrom, usedChannels)
-            allUsers.extend(users) 
+            allUsers.extend(users)
             if allUsers:
                 number = number + addUsersToCsv(allUsers, filepath, True)
             print('peter '+peter+' done and dusted writing')
     print('storing users ended')
     return number
 
+
 def joinChannel(peters, channelName):
     for peter in peters:
         with TelegramClient(peter, api_id, api_hash) as client:
             channelEntity = client.get_entity('t.me'+channelName)
             client(JoinChannelRequest(channelEntity))
-            print('peter '+ peter + 'joined channel' + channelName )
+            print('peter ' + peter + 'joined channel' + channelName)
 
 
-if __name__ == '__main__': 
-    peters = [ 'dynasties','1','10','11','12','13', '5', '7','8','9','akira', 'benjamin', 'chukwu', 'ibe', 'james','john', 'mary','mike','mike10','mike20', 'mike4']
+if __name__ == '__main__':
+    peters = ['dynasties', '1', '10', '11', '12', '13', '5', '7', '8', '9', 'akira',
+              'benjamin', 'chukwu', 'ibe', 'james', 'john', 'mary', 'mike', 'mike10', 'mike20', 'mike4']
     #joinChannel(peters, 'successvisa')
-    add(peters, 'successvisa', getFrom=['wizytech','students_tips', 'TodayILearn', 'american'])
+    add(peters, 'successvisa', getFrom=['TodayILearn', 'american'])
