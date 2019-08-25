974df50b9bfb3a1174d93d61813a2f8499f83c60
dino16m <anselem16m@gmail.com>
2019-08-25 23:45:46 +0100

    hj

--- tele.py
+++ tele.py
@@ -336,4 +336,4 @@ if __name__ == '__main__':
     peters = ['dynasties', '12', '13', '5', '7']
     #joinChannel(peters, 'successvisa')  
     #'james', 'john', 'mary', 'mike', 'mike10', 'mike20', 'mike4'
-    add(peters, 'successvisa', getFrom=['TodayILearn', 'american'])
+    add(peters, 'successvisa', getFrom=[])
