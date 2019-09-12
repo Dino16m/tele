eadc2904feb53e394c8b1a51fa4ae1f3ddd42e35
dino16m <anselem16m@gmail.com>
2019-09-12 08:02:10 +0100

    oodluck

--- tele.py
+++ tele.py
@@ -197,6 +197,7 @@ def add(peters, channelInto, online=True, getFrom=[], filepath='users.txt', limi
                 if not users and count <= limit:
                     return finalResolve('No users to add')
                 if channelInto not in channels.keys():
+                    print('here')
                     joinChannel(client, channelInto)
                     continue
                 if not removedUsersInChannel:
